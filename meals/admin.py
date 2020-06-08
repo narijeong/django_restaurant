e3041e9ad3273e2497d999c9009e68ead29d03bb
Nari Jeong <nari@Naris-MacBook-Pro.local>
2020-06-08 21:53:14 +0900

    add static files

--- meals/admin.py
+++ meals/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Meals
+from .models import Meals, Category
 
 admin.site.register(Meals)
+admin.site.register(Category)
 
 
